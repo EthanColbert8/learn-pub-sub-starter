ffdeda1acf3dc3f035c9cde7f541a3392a4cb079
EthanColbert8 <emcolbert2@gmail.com>
2026-02-02 18:57:31 -0500

    have the server connect to rabbitmq

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	connectionString := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(connectionString)
+	if err != nil {
+		fmt.Printf("Failed to connect to message broker: %v\n", err)
+		return
+	}
+	defer connection.Close()
+
+	fmt.Println("Message broker connection established! Starting Peril server...")
+
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel, os.Interrupt)
+
+	// blocks the main thread until an interrupt
+	<-signalChannel
+	fmt.Println("\nShutdown signal received, closing server.")
+
 }
